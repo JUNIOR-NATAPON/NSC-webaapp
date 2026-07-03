6f8ca6c2db89b6c158c8cbdf94c262b97eb85404
JUNIOR-NATAPON <junior41025@gmail.com>
2026-07-03 13:43:50 +0700

    a

--- clarity-web/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div style={{ padding: "50px", fontSize: "40px" }}>
-      Hello Clarity 🚀
-    </div>
-  );
-}
-
-export default App;
--- clarity-web/src/pages/Signup.jsx
+++
@@ -1,7 +0,0 @@
-export default function Signup() {
-  return (
-    <h1 style={{textAlign:"center",marginTop:"100px"}}>
-      Signup Page
-    </h1>
-  );
-}
