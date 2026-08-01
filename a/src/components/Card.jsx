f69d6bfbfef411862d8ad2f047c92b2c0fcb4503
JUNIOR-NATAPON <junior41025@gmail.com>
2026-08-01 20:44:15 +0700

    clear up

--- a/src/components/Card.jsx
+++
@@ -1,7 +0,0 @@
-export default function Card({ children, className = '' }) {
-  return (
-    <div className={`bg-white dark:bg-card-dark rounded-2xl md:rounded-3xl shadow-card dark:shadow-none p-5 md:p-7 lg:p-8 ${className}`}>
-      {children}
-    </div>
-  )
-}
